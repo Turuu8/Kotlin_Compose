9ebd8c85c17e7ecb8fac117159ca1e5c2b15f2f6
Turuu8 <trmnhtr@gmail.com>
2023-09-04 09:05:55 +0800

    super heroes donw

--- app/src/main/java/com/example/superheroes/HeroesScreen.kt
+++ app/src/main/java/com/example/superheroes/HeroesScreen.kt
@@ -1,77 +1,140 @@
 package com.example.superheroes
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.animation.core.Spring.DampingRatioLowBouncy
+import androidx.compose.animation.core.Spring.StiffnessVeryLow
+import androidx.compose.animation.slideInVertically
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.layout.ContentScale
 import com.example.superheroes.model.HeroesRepository
 import com.example.superheroes.model.Hero
+import com.example.superheroes.ui.theme.SuperHeroesTheme
 
 
-class HeroesScreen {
-}
-
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
-fun SuperHeroesContainer () {
+fun HeroesList(
+    heroes: List<Hero>,
+    modifier: Modifier = Modifier
+) {
+    val visibleState = remember {
+        MutableTransitionState(false).apply {
+            // Start the animation immediately.
+            targetState = true
+        }
+    }
 
-    LazyColumn(){
-        items(HeroesRepository.heroes) {heros->
-            SuperHeroCard(heros)
+    AnimatedVisibility(
+        visibleState = visibleState,
+        enter = fadeIn(
+        animationSpec =  spring(dampingRatio = DampingRatioLowBouncy)) ,
+        exit = fadeOut(),
+        modifier = modifier
+    ) {
+        LazyColumn {
+            itemsIndexed(heroes) { index, hero ->
+                HeroListItem(
+                    hero = hero,
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp, vertical = 8.dp)
+                        // Animate each list item to slide in vertically
+                        .animateEnterExit(
+                            enter = slideInVertically(
+                                animationSpec = spring(
+                                    stiffness = StiffnessVeryLow,
+                                    dampingRatio = DampingRatioLowBouncy
+                                ),
+                                initialOffsetY = { it * (index + 1) } // staggered entrance
+                            )
+                        )
+                )
+            }
         }
     }
 }
 
 @Composable
-fun SuperHeroCard (heroes: Hero, modifier: Modifier = Modifier) {
+fun HeroListItem (hero: Hero, modifier: Modifier = Modifier) {
     Card (
-        shape = RoundedCornerShape(16.dp)
+        modifier = modifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
     ) {
         Row (
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(16.dp)
-                .height(72.dp),
-            horizontalArrangement = Arrangement.SpaceBetween
+                .sizeIn(minHeight = 72.dp)
         ){
-            Column {
+            Column (modifier = Modifier.weight(1f)){
                 Text(
-                    text = stringResource(heroes.nameRes),
+                    text = stringResource(hero.nameRes),
                     style = MaterialTheme.typography.displaySmall
                 )
                 Text(
-                    text = stringResource(heroes.descriptionRes),
+                    text = stringResource(hero.descriptionRes),
                     style = MaterialTheme.typography.bodyLarge
                 )
             }
-            Image(
-                painter = painterResource(heroes.imageRes),
-                contentDescription = null,
+            Spacer(Modifier.width(16.dp))
+            Box (
                 modifier = Modifier
-                    .padding(start = 16.dp)
-                    .clip(MaterialTheme.shapes.small),
-            )
+                    .size(72.dp)
+                    .clip(RoundedCornerShape(8.dp))
+            ){
+                Image(
+                    painter = painterResource(hero.imageRes),
+                    contentDescription = null,
+                    alignment = Alignment.TopCenter,
+                    contentScale = ContentScale.FillWidth
+                )
+            }
         }
     }
 }
-
-@Preview(showBackground = false)
+@Preview("Light Theme")
+@Preview("Heroes List")
 @Composable
-fun SuperHereosScreenPreview () {
-    SuperHeroesContainer()
+fun HeroesPreview() {
+    SuperHeroesTheme(darkTheme = false) {
+        Surface (
+            color = MaterialTheme.colorScheme.background
+        ) {
+            /* Important: It is not a good practice to access data source directly from the UI.
+            In later units you will learn how to use ViewModel in such scenarios that takes the
+            data source as a dependency and exposes heroes.
+            */
+            HeroesList(heroes = HeroesRepository.heroes)
+        }
+    }
 }
--- app/src/main/java/com/example/superheroes/MainActivity.kt
+++ app/src/main/java/com/example/superheroes/MainActivity.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.superheroes.model.HeroesRepository
 import com.example.superheroes.ui.theme.SuperHeroesTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,25 +25,43 @@ class MainActivity : ComponentActivity() {
             SuperHeroesTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    SuperheroesApp()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun SuperheroesApp() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar()
+        }
+    ) {
+        val heroes = HeroesRepository.heroes
+        HeroesList(heroes = heroes, Modifier.padding(it))
+    }
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(title = {
+        Text(
+            text = stringResource(R.string.app_name) ,
+            style = MaterialTheme.typography.displayLarge
+        )
+    },
         modifier = modifier
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun SuperHeroesPreview() {
     SuperHeroesTheme {
-        Greeting("Android")
+        SuperheroesApp()
     }
 }
